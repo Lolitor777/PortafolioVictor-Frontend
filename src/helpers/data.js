7676256b1ff885420dff2a4fbe5ce7cab7cbf2e4
Victor <carvajal57685@gmail.com>
2024-02-03 09:41:28 -0500

    Testimonio modificado

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -10,6 +10,6 @@ export const data = [
     id: 2,
     photo: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
     description: `""`,
-    name: "Nn",
-    job: "La vagancia"
+    name: "",
+    job: ""
 }];
