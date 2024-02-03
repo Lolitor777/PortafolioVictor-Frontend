1db4453d2876061cb13d7b54716d1cb15ccff3b7
Victor <carvajal57685@gmail.com>
2024-02-03 11:49:34 -0500

    Se agrega el testimonio de Juan jose

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -1,15 +1,15 @@
 export const data = [
 {
     id: 1,
-    photo: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+    photo: "https://storage.googleapis.com/imagenesportafolio/photo-gonzalo.jpeg",
     description: `"Como diseñador gráfico, siempre necesité un portafolio web para exhibir mi trabajo. Victor hizo posible esta necesidad, materializando mis ideas a la perfección."`,
     name: "Gonzalo Taborda",
     job: "Diseñador gráfico"
 },
 {
     id: 2,
-    photo: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
-    description: `""`,
-    name: "",
-    job: ""
+    photo: "https://storage.googleapis.com/imagenesportafolio/photo-Porras.png",
+    description: `"Victor se desarrolló bien durante su participación en mi proyecto. Un profesional muy proactivo, altamente recomendado"`,
+    name: "Juan José Porras",
+    job: "Frontend Developer"
 }];
